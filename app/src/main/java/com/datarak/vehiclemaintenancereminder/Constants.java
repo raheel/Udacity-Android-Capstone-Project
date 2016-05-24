440a1ec2ee704efc01039c92e62c8fe74623044a
Raheel Khan <raheel.khan@gmail.com>
2016-05-24 03:41:04 -0400

    removed Edmunds API key

--- app/src/main/java/com/datarak/vehiclemaintenancereminder/Constants.java
+++ app/src/main/java/com/datarak/vehiclemaintenancereminder/Constants.java
@@ -4,6 +4,6 @@ package com.datarak.vehiclemaintenancereminder;
  * Created by raheel on 5/17/16.
  */
 public class Constants {
-    public static final String EDMUNDS_API_KEY = "m276ud66jh53jc9f9cxm682n";
-    public static final String EDMUNDS_API_SECRET = "Hx3UHFENc7tGNf5kP6uFAMqh";
+    public static final String EDMUNDS_API_KEY = "";
+    public static final String EDMUNDS_API_SECRET = "";
 }
