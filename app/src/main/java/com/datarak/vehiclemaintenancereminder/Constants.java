02b6763845aa978d2efa65d6c029f1861e0b0d9b
Raheel Khan <raheel.khan@gmail.com>
2016-05-30 23:34:38 -0400

    removed keys

--- app/src/main/java/com/datarak/vehiclemaintenancereminder/Constants.java
+++ app/src/main/java/com/datarak/vehiclemaintenancereminder/Constants.java
@@ -4,6 +4,7 @@ package com.datarak.vehiclemaintenancereminder;
  * Created by raheel on 5/17/16.
  */
 public class Constants {
+    //removed keys
     public static final String EDMUNDS_API_KEY = "";
     public static final String EDMUNDS_API_SECRET = "";
 }
